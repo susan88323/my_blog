ef7bbee97e484a57a9bb71a4adacaacd9468935f
Susan <wangjm323@gmail.com>
2017-04-13 10:19:38 -0400

    Create action creator, reducer and container to fetch post data

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>
+
+class PostsIndex extends Component {
+  componentWillMount() {
+    // console.log('this is a good time to call an action creator to fetch posts');
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
