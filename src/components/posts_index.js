36d5a0a2400613cdcc83261b114b0902e591d9a6
Susan <wangjm323@gmail.com>
2017-04-13 22:36:06 -0400

    Show list of blogs in post index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -11,6 +11,18 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map(
+            (post) => {
+              return (
+                <li className="list-group-item" key={post.id}>
+                  <strong>{post.title}</strong>
+                  <span className="pull-xs-right">{post.categories}</span>
+                </li>
+              );
+            });
+  }
+
   render() {
     return (
       <div>
@@ -19,14 +31,21 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
 }
 
 // function mapDispatchToProps(dispatch) {
 //   return bindActionCreators({ fetchPosts }, dispatch);
 // }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
